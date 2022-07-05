d846a71c60c270e1963b134aa758f4310e138c30
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-06 02:44:26 +0900

    config: basic typeorm configuration with local mysql

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
-
+import {createConnection} from "typeorm";
 const app = express();
 
 app.get('/api/hello', (req, res) => {
     res.send('hello world');
 })
 
-app.listen(8080, () => {
-    console.log('server is listening 8080');
+// app.listen(8080, () => {
+//     console.log('server is listening 8080');
+// });
+
+createConnection().then(connection => {
+    app.listen(8080, () => {
+        console.log('server is listening 8080');
+    });
 });
