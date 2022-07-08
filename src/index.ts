bba0db729c8645fb5c5012b92debeb248ba99c6e
Woojin Lee <dhkep03@gmail.com>
2022-07-08 22:19:53 +0900

    chore: edit index.ts(comment out code for creation news) [#7]

--- src/index.ts
+++ src/index.ts
@@ -24,42 +24,50 @@ app.use(express.urlencoded({ extended: true }));
 // routes 폴더로 분기
 app.use(routes);
 
-let jeongbuTag1 = new Tag();
-jeongbuTag1.name = '정부';
-let gamyeomTag2 = new Tag();
-gamyeomTag2.name = '감염증';
+
+// News 객체 생성 후 저장하는 부분
+// let jeongbuTag1 = new Tag();
+// jeongbuTag1.name = '정부';
+// let gamyeomTag2 = new Tag();
+// gamyeomTag2.name = '감염증';
+
+// createConnection().then(async (connection) => {
+//   const newsRepository = News.getRepository();
+//   const tagRepository = Tag.getRepository();
+//   let tag1 = tagRepository.create(jeongbuTag1);
+//   let tag2 = tagRepository.create(gamyeomTag2);
+//   tagRepository.save(tag1);
+//   tagRepository.save(tag2);
+
+//   const newsInfo = {
+//     title: '정부, 감염자 격리 의무 한 달 연장',
+//     category: Category.SOCIETY,
+//     tags: [tag1, tag2],
+//     announcerGender: Gender.WOMEN,
+//     channel: Channel.MBC,
+//     link: 'https://www.youtube.com/watch?v=ah6nDLl4_oA&t=1391s',
+//     time: new Time(23, 28),
+//     runningTime: new Time(0, 24),
+//     suitability: Suitability.MEDIUM,
+//     isEmbeddable: true,
+//     reportDate: new Date('2022-05-29'),
+//   };
+//   const news = newsRepository.create(newsInfo);
+//   const news2 = await newsRepository.save(news);
+//   const news3 = await newsRepository.find({
+//     relations: ['tags'],
+//     where: {
+//       id: news2.id,
+//     },
+//   });
+//   console.log('>>>>>>>>>', news3);
+//   app.listen(8080, () => {
+//     console.log('server is listening 8080');
+//   });
+// });
 
 createConnection().then(async (connection) => {
-  const newsRepository = News.getRepository();
-  const tagRepository = Tag.getRepository();
-  let tag1 = tagRepository.create(jeongbuTag1);
-  let tag2 = tagRepository.create(gamyeomTag2);
-  tagRepository.save(tag1);
-  tagRepository.save(tag2);
-
-  const newsInfo = {
-    title: '정부, 감염자 격리 의무 한 달 연장',
-    category: Category.SOCIETY,
-    tags: [tag1, tag2],
-    announcerGender: Gender.WOMEN,
-    channel: Channel.MBC,
-    link: 'https://www.youtube.com/watch?v=ah6nDLl4_oA&t=1391s',
-    time: new Time(23, 28),
-    runningTime: new Time(0, 24),
-    suitability: Suitability.MEDIUM,
-    isEmbeddable: true,
-    reportDate: new Date('2022-05-29'),
-  };
-  const news = newsRepository.create(newsInfo);
-  const news2 = await newsRepository.save(news);
-  const news3 = await newsRepository.find({
-    relations: ['tags'],
-    where: {
-      id: news2.id,
-    },
-  });
-  console.log('>>>>>>>>>', news3);
   app.listen(8080, () => {
-    console.log('server is listening 8080');
+  console.log('server is listening 8080');
   });
 });
