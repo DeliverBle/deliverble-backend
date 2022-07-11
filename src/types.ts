6c6d675ec844a294a320e423e1a0aac1a146b095
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-12 02:18:54 +0900

    feat: findAll property added to ConditionList

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,24 @@ export interface ConditionList {
   channels: boolean;
   categories: boolean;
   announcerGender: boolean;
+  findAll: boolean;
 }
+
+export const hasCategories = (conditionList: ConditionList): boolean => {
+  return conditionList.categories;
+};
+
+export const hasChannels = (conditionList: ConditionList): boolean => {
+  return conditionList.channels;
+};
+
+export const hasAnnouncerGender = (conditionList: ConditionList): boolean => {
+  return conditionList.announcerGender;
+};
+
+export const hasFindAll = (conditionList: ConditionList): boolean => {
+  return conditionList.findAll;
+};
 
 export interface NewsInfo {
   title: string;
