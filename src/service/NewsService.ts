78048f3f75f2a553ba31393dde2e05f2f6bf2093
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-12 01:57:10 +0900

    refactor: remove duplicate declaration of repository

--- src/service/NewsService.ts
+++ src/service/NewsService.ts
@@ -2,30 +2,25 @@ import { getConnection } from 'typeorm';
 import { NewsRepository } from '../repository/NewsRepository';
 import { sortByDate, sortByDateAndTitle, sortByTitle } from '../shared/common/utils';
 import { NewsInfo, SearchCondition } from '../types';
+import { CHANNEL } from '../shared/common/Name';
 
+const connection = getConnection();
+const newsRepository = connection.getCustomRepository(NewsRepository);
 
 const searchAllNews = async () => {
-    const connection = getConnection();
-    const newsRepository = connection.getCustomRepository(NewsRepository);
-    return await newsRepository.find();
-  };
+  return await newsRepository.find();
+};
 
 const searchByChannel = async (searchCondition: SearchCondition) => {
-  const connection = getConnection();
-  const newsRepository = connection.getCustomRepository(NewsRepository);
   return await newsRepository.findByChannels(searchCondition.channels);
 };
 
 const searchByCategory = async (searchCondition: SearchCondition) => {
-  const connection = getConnection();
-  const newsRepository = connection.getCustomRepository(NewsRepository);
   return await newsRepository.findByCategories(searchCondition.categories);
 };
 
 const searchByGender = async (searchCondition: SearchCondition): Promise<NewsInfo[]> => {
   try {
-    const connection = getConnection();
-    const newsRepository = connection.getCustomRepository(NewsRepository);
     return await newsRepository.findByAnnouncerGender(searchCondition.announcerGender);
   } catch (error) {
     console.log(error);
@@ -33,22 +28,23 @@ const searchByGender = async (searchCondition: SearchCondition): Promise<NewsInf
   }
 };
 
-const searchByConditions = async (conditionList: object | boolean, searchCondition: SearchCondition): Promise<NewsInfo[]> | null=> {
-  const connection = getConnection();
-  const newsRepository = connection.getCustomRepository(NewsRepository);
+const searchByConditions = async (
+  conditionList: object | boolean,
+  searchCondition: SearchCondition,
+): Promise<NewsInfo[]> | null => {
   let newsData;
   if (conditionList['channels']) {
     newsData = await newsRepository.findByChannels(searchCondition.channels);
   } else {
     newsData = await newsRepository.findAllNews();
   }
-  
+
   if (conditionList['categories']) {
     newsData = newsData.filter((news) => {
       if (searchCondition.categories.includes(news.category)) {
         return news;
       }
-    })
+    });
   }
 
   if (conditionList['announcerGender']) {
@@ -56,13 +52,13 @@ const searchByConditions = async (conditionList: object | boolean, searchConditi
       if (news.announcerGender === searchCondition.announcerGender) {
         return news;
       }
-    })
+    });
   }
 
   newsData = sortByDateAndTitle(newsData);
 
   return newsData;
-}
+};
 
 export default {
   searchAllNews,
