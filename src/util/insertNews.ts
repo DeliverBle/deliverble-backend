1da6cc9b9d2c5e6f5c9be386651b41440c4c734c
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-13 11:36:26 +0900

    chore: add inline comment for avoiding non-compilation

--- src/util/insertNews.ts
+++ src/util/insertNews.ts
@@ -1,12 +1,11 @@
-import { createConnection, getConnection } from "typeorm";
-import { News } from "../entity/News";
-import { Tag } from "../entity/Tag";
-import { NewsRepository } from "../repository/NewsRepository";
-import { Category } from "../shared/common/Category";
-import { Channel } from "../shared/common/Channel";
-import { Gender } from "../shared/common/Gender";
-import { Suitability } from "../shared/common/Suitability";
-import { Time } from "../vo/Time";
+import { createConnection, getConnection } from 'typeorm';
+import { News } from '../entity/News';
+import { Tag } from '../entity/Tag';
+import { Category } from '../shared/common/Category';
+import { Channel } from '../shared/common/Channel';
+import { Gender } from '../shared/common/Gender';
+import { Suitability } from '../shared/common/Suitability';
+import { Time } from '../vo/Time';
 
 // news 생성하고 포트 열기
 let tagTest1 = new Tag();
@@ -17,11 +16,11 @@ let tagTest3 = new Tag();
 tagTest3.name = '임진강';
 
 export const insertNewsData = async () => {
-    createConnection().then(async (connection) => {
+  createConnection().then(async (connection) => {
     //삭제 ^^
     // const newsRepository = News.getRepository();
     // newsRepository.delete({ id: 193});
-    
+
     const newsRepository = News.getRepository();
     const tagRepository = Tag.getRepository();
     let tag1 = tagRepository.create(tagTest1);
@@ -32,7 +31,7 @@ export const insertNewsData = async () => {
     tagRepository.save(tag2);
 
     const newsInfo = [
-        {
+      {
         title: '북, 최근 임진강 상류 황강댐 수문 개방',
         category: Category.SOCIETY,
         tags: [tag1, tag2, tag3],
@@ -45,16 +44,17 @@ export const insertNewsData = async () => {
         suitability: Suitability.HIGH,
         isEmbeddable: true,
         reportDate: new Date('2022-06-30'),
-        }];
+      },
+    ];
     const news = newsRepository.create(newsInfo);
     const news2 = await newsRepository.save(news);
-    const news3 = await newsRepository.find({
-        relations: ['tags'],
-        where: {
-        id: news2.id,
-        },
-    });
-    console.log('>>>>>>>>>', news3);
-    });
+    // const news3 = await newsRepository.find({
+    //   relations: ['tags'],
+    //   where: {
+    //     id: news2._id,
+    //   },
+    // });
+    // console.log('>>>>>>>>>', news3);
+  });
 };
 // insertNewsData();
