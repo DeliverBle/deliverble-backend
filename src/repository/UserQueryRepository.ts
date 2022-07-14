923a0f4a35634977d145b5aab3c1d0f048aecf04
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 02:28:59 +0900

    refactor: use findByEmail with queryBuilder in user

--- src/repository/UserQueryRepository.ts
+++ src/repository/UserQueryRepository.ts
@@ -1,15 +1,19 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { News } from '../entity/News';
 import { User } from '../entity/User';
+import {Logger} from "tslog";
+
+const log: Logger = new Logger({ name: '딜리버블 백엔드 짱짱' });
 
 @EntityRepository(User)
 export class UserQueryRepository extends Repository<User> {
   // id로 1개의 News 조회
   findById(id: number) {
-    return this.createQueryBuilder('news').where('user.id = :id', { id }).getOne();
+    return this.createQueryBuilder('user').where('user.id = :id', { id }).getOne();
   }
 
   findByEmail(email: string) {
-    return this.createQueryBuilder('news').where('user.email = :email', { email }).getOne();
+    log.debug('email: ', email);
+    return this.createQueryBuilder('user').where('user.email = :email', { email }).getOneOrFail();
   }
 }
