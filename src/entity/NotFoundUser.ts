4036cb63f4fc9d68cdd9f19b43a22954562c4932
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 02:59:57 +0900

    refactor: chagne the validation logic for isNotFoundUser

--- src/entity/NotFoundUser.ts
+++ src/entity/NotFoundUser.ts
@@ -4,13 +4,18 @@ export class NotFoundUser extends User {
   constructor() {
     super("-1", 'User Not Found', '', '분류안됨');
   }
+  isNotFoundUser(): boolean {
+    return true;
+  }
 }
 
-export const isNotFoundUser = (user: User) => {
+export const isNotFoundUser = (_user: User): boolean => {
+  const user = _user as NotFoundUser;
   try {
-    user as NotFoundUser;
+    user.isNotFoundUser();
     return true;
-  } catch (err) {
+    // TODO: 어딘가 좀 이상한 방법... 주객이 전도되었다.. 공부가 부족한 것으로..
+  } catch (e) {
     return false;
   }
 };
