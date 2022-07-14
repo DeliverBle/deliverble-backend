7cb770ac7e77b5406aac917d34eb647a277f2b27
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 00:53:08 +0900

    feat: add UpdatedAccessTokenDTO type

--- src/types.ts
+++ src/types.ts
@@ -91,3 +91,32 @@ export class KakaoRawInfo {
     return new KakaoRawInfo(id, nickname, profile_image, email, Gender.UNSPECIFIED.toString());
   }
 }
+
+export class UpdatedAccessTokenDTO {
+  static NONE_TOKEN = 'NONE';
+  constructor(
+    _access_token: string,
+    _expires_in: string,
+    _refresh_token: string | undefined,
+    _refresh_token_expires_in: string | undefined,
+  ) {
+    this.access_token = _access_token;
+    this.expires_in = _expires_in;
+    this.refresh_token = !_refresh_token ? UpdatedAccessTokenDTO.NONE_TOKEN : _refresh_token;
+    this.refresh_token_expires_in = !_refresh_token_expires_in
+      ? UpdatedAccessTokenDTO.NONE_TOKEN
+      : _refresh_token_expires_in;
+  }
+  access_token: string;
+  expires_in: string;
+  // TODO: NONE이면 반환하지 않는 방법 고민해보기
+  refresh_token?: string;
+  refresh_token_expires_in?: string;
+
+  doesRetrievedRefreshTokenExist(): boolean {
+    return (
+      this.refresh_token !== UpdatedAccessTokenDTO.NONE_TOKEN &&
+      this.refresh_token_expires_in !== UpdatedAccessTokenDTO.NONE_TOKEN
+    );
+  }
+}
