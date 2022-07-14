af6a8bc3d60fac0894b65a38ee848e83ef4ae894
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-14 12:39:45 +0900

    feat: add KakaoRawInfo to kakao-login strategy

--- src/controllers/kakao-login.ts
+++ src/controllers/kakao-login.ts
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import KakaoStrategy from 'passport-kakao';
-import { findUserByEmail, loginUserWithKakao } from '../service/UserService';
-import { isNotFoundUser, NotFoundUser } from '../entity/NotFoundUser';
+import { loginUserWithKakao } from '../service/UserService';
+import { KakaoRawInfo } from '../types';
 
 export default function kakaoLoginStrategy() {
   passport.use(
@@ -12,16 +12,10 @@ export default function kakaoLoginStrategy() {
         callbackURL: process.env.KAKAO_CALLBACK_URL,
       },
       async (accessToken, refreshToken, profile, done) => {
-        const {
-          id,
-          username: name,
-          _json: {
-            properties: { profile_image, nickname, thumbnail_image },
-            kakao_account: { email },
-          },
-        } = profile;
+        const kakaoRawInfo = KakaoRawInfo.toKakaoRawInfo(profile._json);
+        console.log(' >>>>>>>>>>>> kakaoRawinfo', kakaoRawInfo);
         try {
-          await loginUserWithKakao(email);
+          // await loginUserWithKakao(email);
           done(null);
         } catch (err) {
           console.log(err);
