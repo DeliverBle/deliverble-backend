7513569bae1e51bb36c6c95e1f247342d0a7dbc3
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 02:59:30 +0900

    refactor: change findById to findByKakaoId

--- src/repository/UserQueryRepository.ts
+++ src/repository/UserQueryRepository.ts
@@ -1,15 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { News } from '../entity/News';
 import { User } from '../entity/User';
-import {Logger} from "tslog";
+import { Logger } from 'tslog';
 
 const log: Logger = new Logger({ name: '딜리버블 백엔드 짱짱' });
 
 @EntityRepository(User)
 export class UserQueryRepository extends Repository<User> {
   // id로 1개의 News 조회
-  findById(id: number) {
-    return this.createQueryBuilder('user').where('user.id = :id', { id }).getOne();
+  findByKakaoId(kakaoId: string) {
+    return this.createQueryBuilder('user')
+      .where('user.kakaoId = :kakaoId', { kakaoId })
+      .getOneOrFail();
   }
 
   findByEmail(email: string) {
