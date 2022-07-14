1a2f1dec7e7b0761cddeb31fd12236b2923c54eb
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-14 11:35:43 +0900

    feat: add kakaoRouter while change to v2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import NewsRouter from './NewsRouter';
+import passport from 'passport';
+import kakaoRouter from './kakaoRouter';
 
-const VERSION = '/v1';
+const VERSION = '/v2';
 
 const router: Router = Router();
 
 router.use(VERSION + '/news', NewsRouter);
+
+router.use(VERSION + '/auth', kakaoRouter);
 
 export default router;
---
+++ src/routes/kakaoRouter.ts
@@ -0,0 +1,22 @@
+import express, { Router } from 'express';
+import NewsController from '../controllers/NewsController';
+import passport from 'passport';
+import kakaoLoginStrategy from '../controllers/kakao-login';
+
+const router: express.Router = Router();
+
+// register kakao-login strategy
+kakaoLoginStrategy();
+
+router.get('/kakao', passport.authenticate('kakao-login'));
+router.get(
+  '/kakao/oauth',
+  passport.authenticate('kakao-login', {
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    NewsController.callbackKakao(req, res).then((v) => console.log('kakao login succeeded'));
+  },
+);
+
+export default router;
