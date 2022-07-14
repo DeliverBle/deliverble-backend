f22d21f83e893d7af70c6e626b0dbab4ad9e236e
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 02:28:38 +0900

    chore: introduce errorHandler debug

--- src/error/ErrorHandler.ts
+++ src/error/ErrorHandler.ts
@@ -1,6 +1,10 @@
 import CustomError from './CustomError';
+import {Logger} from "tslog";
+
+const log: Logger = new Logger({ name: '딜리버블 백엔드 짱짱' });
 
 export const errorHandler = (fn) => (req, res, next) => {
+  log.warn("ERROR HANDLER", errorHandler)
   Promise.resolve(fn(req, res, next)).catch((err) => {
     if (err instanceof CustomError) {
       res.status(err.code).send({
