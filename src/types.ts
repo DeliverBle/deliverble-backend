6be47edc5d741baef2d657bf243597e99c1f3e0d
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-14 12:39:11 +0900

    feat: add userKakaoRaw dto

--- src/types.ts
+++ src/types.ts
@@ -66,3 +66,28 @@ export class SearchCondition {
     return this.listSize;
   }
 }
+
+export class KakaoRawInfo {
+  constructor(id, nickname, profile_image, email, gender) {
+    this.id = id;
+    this.nickname = nickname;
+    this.profile_image = profile_image;
+    this.email = email;
+    this.gender = gender;
+  }
+
+  id: number;
+  nickname: string;
+  profile_image: string;
+  email: string;
+  gender: string;
+
+  static toKakaoRawInfo(profile) {
+    const {
+      id,
+      properties: { nickname, profile_image },
+      kakao_account: { email },
+    } = profile;
+    return new KakaoRawInfo(id, nickname, profile_image, email, Gender.UNSPECIFIED.toString());
+  }
+}
