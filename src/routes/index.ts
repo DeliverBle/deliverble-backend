e242832ba4c82982398c755e32e96a89ff031d25
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 04:18:14 +0900

    refactor: declare v2 as version2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import NewsRouter from './NewsRouter';
-import passport from 'passport';
 import kakaoRouter from './kakaoRouter';
 
-const VERSION = '/v2';
+const VERSION2 = '/v2';
 
 const router: Router = Router();
 
-router.use(VERSION + '/news', NewsRouter);
+router.use(VERSION2 + '/news', NewsRouter);
 
-router.use(VERSION + '/auth', kakaoRouter);
+router.use(VERSION2 + '/auth', kakaoRouter);
 
 export default router;
