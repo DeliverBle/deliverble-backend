6b3685e938bd4242c26e9eebb4ad3bd16ef5b0aa
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-14 17:43:18 +0900

    feat: update accessToken by using refreshToken

---
+++ src/shared/AuthLink.ts
@@ -0,0 +1,4 @@
+export const OAUTH_TOKEN = 'https://kauth.kakao.com/oauth/token';
+export const CONTENT_TYPE = 'application/x-www-form-urlencoded;charset=utf-8';
+export const REQUEST_RAW_LINK = 'https://kapi.kakao.com/v2/user/me';
+export const ACCESS_TOKEN_INFO = 'https://kapi.kakao.com/v1/user/access_token_info';
