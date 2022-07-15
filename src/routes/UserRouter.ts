f76a643db3b3a5b057f7c70fae924144d27c97d7
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-15 23:45:49 +0900

    feat: add router to have removed favorite news routes

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -8,5 +8,6 @@ const FAVORITE = "/favorite";
 
 router.get(FAVORITE + '/all', errorHandler(UserController.getAllFavoriteNewsList));
 router.post(FAVORITE + '/add', errorHandler(UserController.addFavoriteNews));
+router.post(FAVORITE + '/remove', errorHandler(UserController.removeFavoriteNews));
 
 export default router;
