b7accb6e04e42a94c16cee6b33f82e372c6127e3
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-19 22:18:36 +0900

    feat: add router for query all highlight by kakaoId and newsId

--- src/routes/HighlightRouter.ts
+++ src/routes/HighlightRouter.ts
@@ -4,5 +4,6 @@ import HighlightController from '../controllers/HighlightController';
 const router: express.Router = Router();
 
 router.post('/create', HighlightController.createHighlight);
+router.post('/all', HighlightController.getHighlightByKakaoIdAndNewsId);
 
 export default router;
