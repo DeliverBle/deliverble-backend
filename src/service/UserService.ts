7ad09fb1195bce53f02a0a69d29a62c461d2cf45
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-20 01:24:54 +0900

    fix: check whether accessToken valids or not with given accessToken

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -77,12 +77,20 @@ export const checkAccessTokenExpiryTTLToRedisServer = async (
   userId: string,
 ): Promise<number> => {
   // TODO: validate this logic in controller or additional DTO type class
+  log.debug(" HELLO ")
   if (!accessToken || !userId) {
     log.debug(accessToken, userId)
     throw new ResourceNotFoundError();
   }
   const KEY = ACCESS_TOKEN_PREFIX + userId;
+  log.debug("KEY ", KEY)
+  const getAsync = promisify(redisClient.get).bind(redisClient);
+  const ACCESS_TOKEN_KEY_ON_REDIS = await getAsync(KEY);
+  if (ACCESS_TOKEN_KEY_ON_REDIS !== accessToken) {
+    throw new CustomError(403, "Access Token or Kakao ID is not valid");
+  }
   const ttl = promisify(redisClient.ttl).bind(redisClient);
+  log.debug("TTL ", ttl)
   return await ttl(KEY);
 };
 
@@ -237,6 +245,7 @@ export const loginUserWithKakao = async (
   userId: string,
 ): Promise<UserInfo> => {
   if (await doesAccessTokenExpire(accessToken, userId)) {
+    log.debug("acc, us", accessToken, userId);
     throw new AccessTokenExpiredError();
   }
   const kakaoRawInfo = await getKakaoRawInfo(accessToken, userId);
@@ -363,7 +372,7 @@ const searchByKakaoId = async (kakaoId: string) => {
   } catch {
     log.debug('meow');
     // TODO: need an another handler for this error
-    throw new CustomError(404, "News Not Found");
+    throw new CustomError(404, "User Not Found");
   }
 };
 
