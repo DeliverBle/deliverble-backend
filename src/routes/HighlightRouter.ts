125a46c7f0a805297b1ee2db2982c56ea267148b
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-19 23:58:04 +0900

    refactor: change querying user's highlight of newsId to get method - TODO: not receiving from body but to receive the variable on param

--- src/routes/HighlightRouter.ts
+++ src/routes/HighlightRouter.ts
@@ -4,6 +4,7 @@ import HighlightController from '../controllers/HighlightController';
 const router: express.Router = Router();
 
 router.post('/create', HighlightController.createHighlight);
-router.post('/all', HighlightController.getHighlightByKakaoIdAndNewsId);
+// TODO: BODY로 받지 말고 PARAM QUERY로 받을 것
+router.get('/all', HighlightController.getHighlightByKakaoIdAndNewsId);
 
 export default router;
