cc2729bafb8582ad19ceff424fdb03c9afa8a8a3
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-19 22:19:10 +0900

    refactor: Highlight to have constructor

--- src/entity/Highlight.ts
+++ src/entity/Highlight.ts
@@ -1,32 +1,30 @@
-import {
-    BaseEntity,
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    JoinColumn,
-  } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { User } from './User';
 
-  
-  @Entity()
-  export class Highlight extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id!: number;
-  
-    @ManyToOne((type) => User, {
-      onDelete: "CASCADE"
-    })
-    @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
-    userId!: User;
-  
-    @Column()
-    scriptId: number;
-    
-    @Column()
-    startingIndex: number;
-    
-    @Column()
-    endingIndex: number;        
+@Entity()
+export class Highlight extends BaseEntity {
+  constructor(_user: User, _scriptId: number, _startingIndex: number, _endingIndex: number) {
+    super();
+    this.user = _user;
+    this.scriptId = _scriptId;
+    this.startingIndex = _startingIndex;
+    this.endingIndex = _endingIndex;
   }
-  
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne((type) => User, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user!: User;
+
+  @Column()
+  scriptId: number;
+
+  @Column()
+  startingIndex: number;
+
+  @Column()
+  endingIndex: number;
+}
