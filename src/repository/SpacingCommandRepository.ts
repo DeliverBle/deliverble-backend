d49d312c4f52ad1e253d3c6f9680b1de793a3a11
Woojin Lee <dhkep03@gmail.com>
2022-07-20 16:24:21 +0900

    feat: add removeSpacingBySpacingId in Repository

--- src/repository/SpacingCommandRepository.ts
+++ src/repository/SpacingCommandRepository.ts
@@ -1,11 +1,23 @@
+import { Logger } from "tslog";
 import {EntityRepository, Repository} from "typeorm";
 import { Spacing } from "../entity/Spacing";
 
+const log: Logger = new Logger({ name: '딜리버블 백엔드 짱짱' });
 
 @EntityRepository(Spacing)
 export class SpacingCommandRepository extends Repository<Spacing> {
   async saveNewSpacing(spacing: Spacing): Promise<Spacing> {
     const newSpacing = this.create(spacing);
     return this.save(newSpacing);
-  }
+  };
+
+  async removeSpacingBySpacingId(spacingId: number): Promise<boolean> {
+    try {
+      await this.delete({id: spacingId});
+      return true;
+    } catch (err) {
+      log.error(err);
+      return false;
+    }
+  };
 }
