d349a32d576383250e0e2393f590a16392ba6668
Woojin Lee <dhkep03@gmail.com>
2022-07-20 16:16:41 +0900

    feat: add removeSpacing DTO

--- src/types.ts
+++ src/types.ts
@@ -391,3 +391,21 @@ export class SpacingReturnCollectionDTO {
     return this;
   }
 }
+
+export class RemoveSpacing {
+  constructor(
+    _accessToken: string,
+    _kakaoId: string,
+    _newsId: number,
+    _spacingId: number,
+    ) {
+    this.accessToken = _accessToken;
+    this.kakaoId = _kakaoId;
+    this.newsId = _newsId;
+    this.spacingId = _spacingId;
+  }    
+  accessToken: string;
+  kakaoId: string;
+  newsId: number;
+  spacingId: number;
+}
