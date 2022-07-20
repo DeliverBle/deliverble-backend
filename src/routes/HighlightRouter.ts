ebe5ef1cf4578df562bbd2f27d3f430372508201
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-20 16:50:55 +0900

    feat: render update path on Highlight Router

--- src/routes/HighlightRouter.ts
+++ src/routes/HighlightRouter.ts
@@ -2,13 +2,15 @@ import express, { Router } from 'express';
 import HighlightController, {
   removeExistingMemoOfHighlight,
   removeHighlightByKakaoIdAndHighlightId,
+  updateExistingMemoOfHighlight,
 } from '../controllers/HighlightController';
 
 const router: express.Router = Router();
 
 const MEMO: string = '/memo';
 
-router.post(MEMO + '/remove', HighlightController.removeExistingMemoOfHighlight)
+router.post(MEMO + '/update', HighlightController.updateExistingMemoOfHighlight);
+router.post(MEMO + '/remove', HighlightController.removeExistingMemoOfHighlight);
 router.post(MEMO + '/add', HighlightController.addNewMemoOfHighlight);
 router.post('/remove', HighlightController.removeHighlightByKakaoIdAndHighlightId);
 router.post('/create', HighlightController.createHighlight);
